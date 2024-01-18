29ed868014908fdf33d830c56c58eb9dc47c7ba6
debops123 <devorah.cohen@post.runi.ac.il>
2024-01-18 22:34:08 +0200

    question 2

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,107 +1,91 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        int[] array = {1, 3, 5, 7, 9};
-        int[] array2 = {1, 3, 5, 7, 9};
-        
-        System.out.println("Missing Int: " + findMissingInt(array));
-        System.out.println("Second Max Value: " + secondMaxValue(array));
-        System.out.println("Contains The Same Elements: " + containsTheSameElements(array, array2));
-        System.out.println("Is Sorted: " + isSorted(array));
+
+        int[] testCase1 = {2,8,3,7,8};
+
+        System.out.println(secondMaxValue(testCase1));
     }
 
-    public static int findMissingInt(int[] array) {
-        if (array == null || array.length == 0) {
-            return -1;
-        }
-
-        int n = array.length + 1;
-        int expectedSum = n * (n + 1) / 2;
-        int actualSum = sumArray(array);
-
-        return expectedSum - actualSum;
-    }
-
-    public static int secondMaxValue(int[] array) {
-        if (array == null || array.length < 2) {
-            return 0; 
-        }
-
-        int max = Integer.MIN_VALUE;
-        int secondMax = Integer.MIN_VALUE;
-
-        for (int num : array) {
-            if (num > max) {
-                secondMax = max;
-                max = num;
-            } else if (num > secondMax && num < max) {
-                secondMax = num;
-            }
-        }
-
-        return secondMax;
-    }
-
-    // Check if two arrays contain the same elements
-    public static boolean containsTheSameElements(int[] array1, int[] array2) {
-        if (array1 == null || array2 == null) {
-            return false;
-        }
-
-        if (array1.length != array2.length) {
-            return false;
-        }
-
-        int[] sortedArray1 = copyAndSort(array1);
-        int[] sortedArray2 = copyAndSort(array2);
-
-        for (int i = 0; i < sortedArray1.length; i++) {
-            if (sortedArray1[i] != sortedArray2[i]) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static boolean isSorted(int[] array) {
-        if (array == null || array.length < 2) {
-            return true; 
-        }
-
-        for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] > array[i + 1]) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private static int sumArray(int[] array) {
-        int sum = 0;
-        for (int num : array) {
-            sum += num;
-        }
-        return sum;
-    }
-
-    private static int[] copyAndSort(int[] array) {
-        int[] copy = new int[array.length];
-        System.arraycopy(array, 0, copy, 0, array.length);
-        bubbleSort(copy);
-        return copy;
-    }
-
-    private static void bubbleSort(int[] array) {
+    public static int findMissingInt (int[] array) {
         int n = array.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+
+        for (int i = 0; i <= n; i++) {
+            boolean number = false;
+            for (int index = 0; index < array.length; index++) {
+                if (array[index] == i) {
+                    number = true;
                 }
             }
+            if (number == false) {
+               return i;
+            }
         }
+        return 1;
+    }
+
+    public static int secondMaxValue(int [] array) {
+        int l = array.length;
+        int large = 0;
+        int secondLarger = 0;
+
+        for (int i = 0; i < l; ++i) {
+            if (array[i] > secondLarger) {
+                secondLarger = large;
+                large = array[i];
+            }
+        }
+        return secondLarger;
+    }
+
+
+    public static boolean containsTheSameElements(int [] array1,int [] array2) {
+        int n = array1.length;
+        int m = array2.length;
+
+        for (int i = 1; i < n; i++ ) { 
+           for (int j = 0; j < m; j++) { 
+            if (array1[i] == array2[j]) {
+                break;
+            }
+            else if (j == (m - 1)) {
+                return false;
+            }
+           }
+        }
+
+        for (int i = 1; i < m; i++ ) { 
+            for (int j = 0; j < n; j++) { 
+             if (array2[i] == array1[j]) {
+                 break;
+             }
+             else if (j == (n - 1)) {
+                 return false;
+             }
+            }
+         }
+        return true;
+    }
+
+
+
+    public static boolean isSorted(int [] array) {
+        // Write your code here:
+        if (array.length <= 1) {
+            return true;
+        }
+
+        boolean minOrMax = array[0] < array[1];
+
+        for (int i = 1; i < array.length; i++) {
+            if (minOrMax) {
+                if (array[i - 1] > array[i]) {
+                    return false;
+                }
+            } else if (array[i - 1] < array[i]) {
+                    return false;
+                }
+            }
+            return true;
+
     }
 }
--- StringOps.java
+++ StringOps.java
@@ -1,94 +1,89 @@
-public class StringOps {
-    ////////////////////////////////////////////////////////////
-    //////                                               ///////
-    //////              Reminder:                        ///////
-    //////        allowed methods                        ///////
-    //////                                               ///////
-    //////        1.charAt(int index)                    ///////
-    //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
-    //////                                               ///////
-    //////        The rest are not allowed !             ///////
-    //////        if you want to use a different         ///////
-    //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
-    //////        you need to implement a version of     ///////
-    //////        the function by yourself.              ///////
-    //////                                               ///////
-    //////        see example for substring              ///////
-    //////        in Recitation 3 question 5             ///////
-    //////                                               ///////
-    ////////////////////////////////////////////////////////////
-    public static void main(String[] args) {
-        String input = "Hello, World!";
-        
-        System.out.println("Capitalize Vowels, Lowercase Rest: " + capVowelsLowRest(input));
-        System.out.println("Camel Case: " + camelCase(input));
-        System.out.println("All Indexes of 'o': " + Arrays.toString(allIndexOf(input, 'o')));
+public static void main(String[] args) {
+	private static char characterToLowerCase(char c) {
+        if (c >= 'A' && c <= 'Z') {
+            return (char)(c + 32);
+        }
+        return c;
     }
 
-    public static String capVowelsLowRest(String string) {
-        StringBuilder result = new StringBuilder();
+    private static char characterToUpperCase(char c) {
+        if (c >= 'a' && c <= 'z') {
+            return (char)(c - 32);
+        }
+        return c;
+    }
 
-        for (int i = 0; i < string.length(); i++) {
-            char currentChar = string.charAt(i);
-            if (isVowel(currentChar)) {
-                result.append(Character.toUpperCase(currentChar));
+    private static boolean characterIsVowel(char c) {
+        switch (c) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public static String capVowelsLowRest (String string) {
+        String result = "";
+
+        for (int i = 0; i < string.length(); ++i) {
+            if (characterIsVowel(string.charAt(i))) {
+                result += characterToUpperCase(string.charAt(i));
             } else {
-                result.append(Character.toLowerCase(currentChar));
+                result += characterToLowerCase(string.charAt(i));
             }
         }
-
-        return result.toString();
-    }
-
-    public static String camelCase(String string) {
-        StringBuilder result = new StringBuilder();
-
-        boolean capitalizeNext = false;
-
-        for (int i = 0; i < string.length(); i++) {
-            char currentChar = string.charAt(i);
-
-            if (Character.isLetter(currentChar)) {
-                if (capitalizeNext) {
-                    result.append(Character.toUpperCase(currentChar));
-                    capitalizeNext = false;
-                } else {
-                    result.append(Character.toLowerCase(currentChar));
-                }
-            } else {
-                capitalizeNext = true;
-            }
-        }
-
-        return result.toString();
-    }
-
-    public static int[] allIndexOf(String string, char chr) {
-        int count = 0;
-
-        for (int i = 0; i < string.length(); i++) {
-            if (string.charAt(i) == chr) {
-                count++;
-            }
-        }
-
-        int[] result = new int[count];
-        int index = 0;
-
-        for (int i = 0; i < string.length(); i++) {
-            if (string.charAt(i) == chr) {
-                result[index++] = i;
-            }
-        }
-
         return result;
     }
 
-    private static boolean isVowel(char c) {
-        return "AEIOUaeiou".indexOf(c) != -1;
+    public static String camelCase (String string) {
+        String result = "";
+
+        for (int i = 0; i < string.length(); ++i) {
+            while (string.charAt(i) == ' ') {
+                ++i;
+            }
+
+            // we found first character already, make first character
+            // of new word uppercase
+            if (result != "" && string.charAt(i - 1) == ' ') {
+                result += characterToUpperCase(string.charAt(i));
+            } else {
+                result += characterToLowerCase(string.charAt(i));
+            }
+        }
+        return result;
+    }
+    public static int[] allIndexOf (String string, char chr) {
+        int indexOfCharacter = -1;
+        int indexesFound = 0;
+
+        do {
+            indexOfCharacter = string.indexOf(chr, indexOfCharacter + 1);
+            if (indexOfCharacter != -1) {
+                indexesFound++;
+            }
+        } while (indexOfCharacter != -1);
+
+
+        int[] result = new int[indexesFound];
+        int indexIndex = 0;
+
+        do {
+            indexOfCharacter = string.indexOf(chr, indexOfCharacter + 1);
+            if (indexOfCharacter != -1) {
+                result[indexIndex++] = indexOfCharacter;
+            }
+        } while (indexOfCharacter != -1); 
+
+        return result;
     }
 }
