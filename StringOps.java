0d9768b5b3faa53e1b0f86136c90f10c4b5e9dc0
debops123 <devorah.cohen@post.runi.ac.il>
2024-01-18 08:50:12 +0200

    question 2

--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,73 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
+        String input = "Hello, World!";
         
+        System.out.println("Capitalize Vowels, Lowercase Rest: " + capVowelsLowRest(input));
+        System.out.println("Camel Case: " + camelCase(input));
+        System.out.println("All Indexes of 'o': " + Arrays.toString(allIndexOf(input, 'o')));
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+    public static String capVowelsLowRest(String string) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < string.length(); i++) {
+            char currentChar = string.charAt(i);
+            if (isVowel(currentChar)) {
+                result.append(Character.toUpperCase(currentChar));
+            } else {
+                result.append(Character.toLowerCase(currentChar));
+            }
+        }
+
+        return result.toString();
     }
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String camelCase(String string) {
+        StringBuilder result = new StringBuilder();
+
+        boolean capitalizeNext = false;
+
+        for (int i = 0; i < string.length(); i++) {
+            char currentChar = string.charAt(i);
+
+            if (Character.isLetter(currentChar)) {
+                if (capitalizeNext) {
+                    result.append(Character.toUpperCase(currentChar));
+                    capitalizeNext = false;
+                } else {
+                    result.append(Character.toLowerCase(currentChar));
+                }
+            } else {
+                capitalizeNext = true;
+            }
+        }
+
+        return result.toString();
     }
 
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+    public static int[] allIndexOf(String string, char chr) {
+        int count = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                count++;
+            }
+        }
+
+        int[] result = new int[count];
+        int index = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                result[index++] = i;
+            }
+        }
+
+        return result;
+    }
+
+    private static boolean isVowel(char c) {
+        return "AEIOUaeiou".indexOf(c) != -1;
     }
 }
